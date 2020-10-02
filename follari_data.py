3ab165278ce72b70e6bb1cc6140bd13a66806e9c
hepahe <henna.heinajarvi@gmail.com>
2020-10-02 17:22:53 +0300

    simplified code, grouped results

--- follari_data.py
+++ follari_data.py
@@ -1,18 +1,26 @@
 import pandas as pd
+import matplotlib.pyplot as plt
 
 follari_data= pd.read_csv("kaupunkipyoratilasto.csv")
 
-follari_jan_2019 = follari_data[follari_data["month"] == "2019-01"]
-follari_feb_2019 = follari_data[follari_data["month"] == "2019-02"]
-follari_mar_2019 = follari_data[follari_data["month"] == "2019-03"]
-follari_apr_2019 = follari_data[follari_data["month"] == "2019-04"]
-follari_may_2019 = follari_data[follari_data["month"] == "2019-05"]
-follari_jun_2019 = follari_data[follari_data["month"] == "2019-06"]
-follari_jul_2019 = follari_data[follari_data["month"] == "2019-07"]
-follari_aug_2019 = follari_data[follari_data["month"] == "2019-08"]
-follari_sep_2019 = follari_data[follari_data["month"] == "2019-09"]
-follari_oct_2019 = follari_data[follari_data["month"] == "2019-10"]
-follari_nov_2019 = follari_data[follari_data["month"] == "2019-11"]
-follari_dec_2019 = follari_data[follari_data["month"] == "2019-12"]
+all_trips_2019 = follari_data[(follari_data["month"] >= "2019-01") & (follari_data["month"] <= "2019-12")]
 
-print(follari_dec_2019.head())
+#removing trips which started and ended at the same stop
+
+outbound_trips = all_trips_2019[all_trips_2019.start_place_name != all_trips_2019.end_place_name]
+
+end_destinations = outbound_trips.groupby(["start_place_name","end_place_name"]).sum()
+end_destinations = end_destinations.sort_values(["start_place_name","count","end_place_name"],ascending=[True,False,False])
+
+start_place_popularity = outbound_trips.groupby("start_place_name").sum() 
+start_place_popularity = start_place_popularity.sort_values("count",ascending=False)
+
+end_place_popularity = outbound_trips.groupby("end_place_name").sum()
+end_place_popularity = end_place_popularity.sort_values("count",ascending=False)
+
+print(start_place_popularity.head())
+print(end_place_popularity.head())
+
+# graphs
+start_place_popularity.plot(kind="bar")
+plt.show()
